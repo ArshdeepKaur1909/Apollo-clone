ceb115ee42cb56db984c777859f9f5d0992b1252
Arshdeep <harkiratkaur8c6@gmail.com>
2025-04-28 19:13:37 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+
+app.use(express.urlencoded(({extended: true})));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.listen(8080, () => {
+    console.log("Listening Started");
+});
